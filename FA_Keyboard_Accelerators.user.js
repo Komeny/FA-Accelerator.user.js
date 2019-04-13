7e388651df50baf2b2cea763578773a78d4a3e18
Komeny <jiomeny@gmail.com>
2019-04-13 04:59:46 +0200

    Remove lightbox class to prevent FA styles

--- FA_Keyboard_Accelerators.user.js
+++ FA_Keyboard_Accelerators.user.js
@@ -262,7 +262,7 @@ var Slideshow = (function() {
 
 		// initialisations
 		lightbox = $(`
-		<div id="fa_accelerate_lightbox" class="fa_accelerate_lightbox lightbox">
+		<div id="fa_accelerate_lightbox" class="fa_accelerate_lightbox">
 			<svg style="position: absolute; width: 0; height: 0; overflow: hidden" version="1.1"
 					xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 				<defs>
